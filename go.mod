2fef405ee7511aa1a4745200abe1cd75dc98be0e
= <nguyenngocha100602@gmail.com>
2024-08-18 15:18:45 +0700

    update

---
+++ go.mod
@@ -0,0 +1,12 @@
+module golang-socketio
+
+go 1.22.1
+
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/googollee/go-socket.io v1.7.0 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/gorilla/handlers"
+)
+
 func main() {
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("Connected: ", s.ID())
+		
+		return nil
+	})
+
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("Notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error){
+		fmt.Println("Error: ", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		// server.Remove(s.ID())
+		fmt.Println("Closed: ", reason)
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	corsMiddleware := handlers.CORS(handlers.AllowedOrigins([]string{"http://localhost:3001"}))
+
+	http.Handle("/socket.io/", corsMiddleware(server))
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
